780b9818e0ce76ae3bba4cd85eca1828b54cf6e0
Jorge Wadley Saraiva <126410409+KuroJW@users.noreply.github.com>
2024-09-23 21:32:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function Menu()
+{
+    var menuBt = document.getElementById("menu-list");
+
+    menuBt.classList.toggle("hide");
+}
+
+// "Toglle" do inglês, significa "Alternar", neste caso ao clicar no bot]ao vai remover a classe, e ao clicar novamente irá adicionar a classe
